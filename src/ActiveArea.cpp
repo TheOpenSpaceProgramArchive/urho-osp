TheOpenSpaceProgramArchive/urho-osp

commit 5ff140abcff7f2301e846bbf90b4cb2c47c637aa

Improve the universe data structure
Changes made here tends to the flaws described in the previous
commit (9852895), working towards a better system to manage a
very large universe.

* Updated Satellite class
  * Implemented calculate_relative_position()
  * Added add_child() function, now it's a working n-ary tree
  * Added m_name
  * Added pointer array for children
* Updated AstronomicalBody class
  * Implemented load(), it creates a Node that has PlanetTerrain
* Updated OspUniverse::debug_function
  * Loads 5 carefully arranged AstronomicalBody satellites,
    structured in a way to test the new coordinate system.
    They are visible in-game.
  * Bug: Destructing the root Satellite causes a crash
* Removed Entity class
* Cleaned up main.cpp
  * Removed unused event handlers
  * Removed large blocks of commented code
* Added more comments to every file changed
* A few other changes

src/ActiveArea.cpp
@@ -1,3 +1,4 @@
+#include <Urho3D/Core/Context.h>
 #include <Urho3D/Physics/PhysicsWorld.h>
 #include <Urho3D/Physics/CollisionShape.h>
 
@@ -9,7 +10,12 @@ using namespace Urho3D;
 
 ActiveArea::ActiveArea(Context* context) : LogicComponent(context)
 {
+    m_name = "Untitled ActiveArea";
+}
 
+ActiveArea::~ActiveArea()
+{
+    //Satellite::~Satellite();
 }
 
 void ActiveArea::RegisterObject(Context* context)
@@ -19,6 +25,8 @@ void ActiveArea::RegisterObject(Context* context)
 
 void ActiveArea::FixedUpdate(float timeStep)
 {
+    m_activeNode = node_;
+    
     PhysicsWorld* pw = node_->GetScene()->GetComponent<PhysicsWorld>();
 
     LongVector3 focusPosLong;
@@ -86,12 +94,12 @@ void ActiveArea::FixedUpdate(float timeStep)
     pw->SetGravity(gravity);
 }
 
-void ActiveArea::load(ActiveArea* area) const
+void ActiveArea::load(ActiveArea* area)
 {
-
+    URHO3D_LOGINFOF("hey");
 }
 
-void ActiveArea::unload() const
+void ActiveArea::unload()
 {
-
+    URHO3D_LOGINFOF("hey");
 }

src/ActiveArea.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include "LongVector3.h"
-#include "OspUniverse.h"
+#include <Urho3D/Scene/LogicComponent.h>
+
 #include "Satellite.h"
 
 using namespace Urho3D;
 
 namespace osp {
+
+class ActiveArea;
 
 /**
  * Turns an ordinary Urho3D scene into a window to the larger OSP universe
@@ -17,7 +19,10 @@ class ActiveArea : public LogicComponent, public Satellite
     URHO3D_OBJECT(ActiveArea, LogicComponent)
 
 public:
+
     ActiveArea(Context* context);
+    ~ActiveArea();
+
     virtual void FixedUpdate(float timeStep) override;
 
     static void RegisterObject(Context* context);
@@ -27,8 +32,8 @@ public:
     Satellite* get_focus() const { return m_focus; };
     void set_focus(Satellite* sat) { m_focus = sat; };
 
-    void load(ActiveArea *area) const override;
-    void unload() const override;
+    void load(ActiveArea *area) override;
+    void unload() override;
 
 private:
 

src/AstronomicalBody.cpp
@@ -1,16 +1,46 @@
 #include <Urho3D/Core/Context.h>
+#include <Urho3D/Container/Ptr.h>
 
+#include "ActiveArea.h"
 #include "AstronomicalBody.h"
+#include "PlanetTerrain.h"
 
 using namespace osp;
 
-
-void AstronomicalBody::load(ActiveArea* area) const
+AstronomicalBody::AstronomicalBody()
 {
-
+    m_radius = 4000.0f;
+    m_name = "Untitled";
 }
 
-void AstronomicalBody::unload() const
+AstronomicalBody::~AstronomicalBody()
 {
+    //Satellite::~Satellite();
+}
 
+void AstronomicalBody::load(ActiveArea* area)
+{
+    Node* scene = area->GetNode();
+
+    m_activeNode = scene->CreateChild(m_name);
+    PlanetTerrain* terrain = m_activeNode->CreateComponent<PlanetTerrain>();
+    terrain->initialize(this);
+
+    LongVector3 relativePos = Satellite::calculate_relative_position(
+                                        area, this, m_unitsPerMeter);
+
+    Vector3 floatPos;
+    floatPos.x_ = float(relativePos.x_) / m_unitsPerMeter;
+    floatPos.y_ = float(relativePos.y_) / m_unitsPerMeter;
+    floatPos.z_ = float(relativePos.z_) / m_unitsPerMeter;
+
+    URHO3D_LOGINFOF("Center at: %f %f %f", floatPos.x_,
+                    floatPos.y_, floatPos.z_);
+
+    m_activeNode->SetPosition(floatPos);
+}
+
+void AstronomicalBody::unload()
+{
+    m_activeNode->Remove();
 }

src/AstronomicalBody.h
@@ -14,23 +14,22 @@ class AstronomicalBody : public Satellite
 
 public:
 
-    AstronomicalBody()
-    {
-
-    };
+    AstronomicalBody();
+    ~AstronomicalBody() override;
 
     float get_radius()
     {
         return m_radius;
     }
 
-    void load(ActiveArea *area) const override;
+    void load(ActiveArea *area) override;
 
-    void unload() const override;
+    void unload() override;
 
 private:
 
-    float m_radius; // sea level
+    // Minimum height, for now
+    float m_radius;
 
 };
 

src/OspUniverse.cpp
@@ -11,7 +11,6 @@
 #include <Urho3D/Resource/ResourceCache.h>
 
 #include "ActiveArea.h"
-#include "Entity.h"
 #include "MachineRocket.h"
 #include "PlanetTerrain.h"
 #include "OspUniverse.h"
@@ -105,10 +104,6 @@ OspUniverse::OspUniverse(Context* context) : Object(context)
     }
 }
 
-/**
- * @brief Performs many other functions
- * @param which [in] Hash for which function to call
- */
 void OspUniverse::debug_function(const StringHash which)
 {
     // Get scene
@@ -129,30 +124,61 @@ void OspUniverse::debug_function(const StringHash which)
     }
     else if (which == StringHash("create_universe"))
     {
+        URHO3D_LOGINFOF("Creating universe...");
+
+        // Destroy the universe first
+        m_bigUniverse = nullptr;
+
+        // AstronomicalBody is currently hard-coded to default to a very dense
+        // astroid-sized ridiculously spherical 4km radius bumpy ball
+
         // Set the root of the universe to a single body
         m_bigUniverse = new AstronomicalBody();
 
-        // AstronomicalBody is currently hard-coded to default to a very dense
-        // astroid-sized ridiculously spherical bumpy ball
+        // Add some more AstronomicalBody
 
-        // Make the scene a part of the new solar system
+        AstronomicalBody* moonA = new AstronomicalBody();
+        moonA->set_position(LongVector3(1024 * 16000, 0, 0));
+        m_bigUniverse->add_child(moonA);
+
+        AstronomicalBody* moonB = new AstronomicalBody();
+        moonB->set_position(LongVector3(-1024 * 16000, 0, 0));
+        m_bigUniverse->add_child(moonB);
+        
+        AstronomicalBody* moonBA = new AstronomicalBody();
+        moonBA->set_position(LongVector3(0, 0, 1024 * 16000));
+        moonB->add_child(moonBA);
+
+        AstronomicalBody* moonBAA = new AstronomicalBody();
+        moonBAA->set_position(LongVector3(0, 0, 1024 * 16000));
+        moonBA->add_child(moonBAA);
+
+        // test calculation for relative position
+        LongVector3 d;
+        d = Satellite::calculate_relative_position(moonA, moonB, 1024);
+        URHO3D_LOGINFOF("moonA->moonB: %i %i %i", d.x_, d.y_, d.z_);
+
+        // Link the Urho scene to the universe just created
         ActiveArea* area = scene->CreateComponent<ActiveArea>();
-        //area->relocate(ab, LongVector3(0, 4150 * 1000, 0));
-        //area->set_focus(scene->GetChild("Subject")->GetComponent<Entity>());
-        //area->set_terrain(terrain);
+
+        m_bigUniverse->add_child(area);
+
+        // Position 150m above the surface
+        area->set_position(LongVector3(0, 4150 * 1024, 0));;
+
+        // Load them into the area
+        // Usually these functions should be called by the ActiveArea itself
+        // but any sort of distance checking auto loading isn't implemented yet
+        m_bigUniverse->load(area);
+        moonA->load(area);
+        moonB->load(area);
+        moonBA->load(area);
+        moonBAA->load(area);
 
     }
 
 }
 
-/**
- * @brief Adds an OSP resource directory
- * @param path [in] Absolute File Path to directory
- *
- * The directory contents will be scanned for parts and scripts.
- * Contents of a subdirectory named "Parts" will be scanned recursively for .gltf.sturdy files
- *
- */
 void OspUniverse::process_directory(const String& path)
 {
     ResourceCache* cache = GetSubsystem<ResourceCache>();
@@ -190,10 +216,6 @@ void OspUniverse::process_directory(const String& path)
     }
 }
 
-/**
- * @brief Include a .gltf.sturdy's contents into the categories of parts
- * @param gltf [in] GLTF file to process
- */
 void OspUniverse::register_parts(const GLTFFile* gltf)
 {
     // Doesn't need to be an actual scene, just make it a node
@@ -336,10 +358,6 @@ void OspUniverse::part_node_recurse(Node* partRoot, Node* node)
     }
 }
 
-/**
- * @brief OspUniverse::make_craft A function that adds Entity and some functionality to a node made of disabled parts. currently just adds Entity
- * @param node [in, out] Node to turn into a craft
- */
 void OspUniverse::make_craft(Node* node)
 {
     //node->CreateComponent<Entity>();

src/OspUniverse.h
@@ -43,13 +43,43 @@ public:
 
     Scene* get_hidden_scene() { return m_hiddenScene; }
 
+    /**
+     * Various debug functions that can be called from AngelScript
+     * @param which [in] Hash for which function to call
+     */
     void debug_function(const StringHash which);
+
+    /**
+     * @brief Adds an OSP resource directory
+     * @param path [in] Absolute File Path to directory
+     *
+     * The directory contents will be scanned for parts and scripts.
+     * Contents of a subdirectory named "Parts" will be scanned recursively for .gltf.sturdy files
+     *
+     */
     void process_directory(const String& path);
+
+    /**
+     * @brief Include a .gltf.sturdy's contents into the categories of parts
+     * @param gltf [in] GLTF file to process
+     */
     void register_parts(const GLTFFile* gltf);
+
     //void register_machine(ObjectFactory* factory);
+
+    /**
+     * This function shouldn't exist
+     * @param node Yes
+     */
     void make_craft(Node* node);
     
 private:
+
+    /**
+     * Recursive function used to scan through sturdy files
+     * @param partRoot glTF root that should be passed on unchanged
+     * @param node Current node
+     */
     void part_node_recurse(Node* partRoot, Node* node);
 };
 

src/Satellite.cpp
@@ -1,11 +1,66 @@
+#include <Urho3D/IO/Log.h>
+
 #include "Satellite.h"
 
 using namespace osp;
 
-// something goes here
-
-LongVector3 Satellite::calculate_relative_position(const LongVector3& to,
-                                       unsigned unitsPerMeter) const
+Satellite::~Satellite()
 {
-    return LongVector3(0, 0, 0);
+    URHO3D_LOGINFOF("satellite destroyed!");
+}
+
+void Satellite::add_child(Satellite *newChild)
+{
+    if (newChild->m_parent.NotNull())
+    {
+        // TODO: move old parent's UniquePtr child to this satellite
+    }
+    newChild->m_depth = m_depth + 1;
+    newChild->m_parent = this;
+    m_children.Push(UniquePtr<Satellite>(newChild));
+}
+
+
+LongVector3 Satellite::calculate_relative_position(const Satellite* from,
+                                                const Satellite* to,
+                                                unsigned unitsPerMeter)
+{
+    // Distance to self will never be greater than or less than zero.
+    if (from == to)
+    {
+        //URHO3D_LOGERRORF("Satellite calculating own position");
+        return LongVector3::ZERO;
+    }
+    
+    // Special case where their parents are the same
+    //if (from->m_parent == to->m_parent)
+    //{
+    //    
+    //}
+    
+    // Relative position for satellites with different parents
+    // Similar to "N-ary Tree Least Common Ancestor"
+    
+    const Satellite* satA = from;
+    const Satellite* satB = to;
+    
+    LongVector3 posA;
+    LongVector3 posB;
+
+    while (satA != satB)
+    {
+        if (satA->m_depth <= satB->m_depth)
+        {
+            posB += satB->m_position;
+            satB = satB->m_parent;
+        }
+        else
+        {
+            posA += satA->m_position;
+            satA = satA->m_parent;
+        }
+        
+    }
+    
+    return posB - posA;
 }

commit 3d94177cac4fcc1bc5d9ea7256cb8f00cd594dc0

Set Object as the base class of Satellite
Making Satellite an Object allows it to receive Urho3D event
handlers and have type identification, while still having the
same reference counting. This was done mainly because of a
diamond problem with ActiveArea, inheriting RefCounted twice
through LogicComponent and Satellite. This caused the crash
described in commit (5ff140a). Inheritance to LogicComponent was
removed, and Satellite was changed to an Object so that
ActiveArea can receive to events from the scene.

Notable changes:

* Updated Satellite class
  * Base class changed to Object, and updated it and all of it's
    derived classes
  * Replaced m_unitsPerMeter with m_precision, see comments
  * Added is_loaded() function
  * Added calculate_position() function, see comments
  * Implemented load() for derived classes to call
* A few other changes

src/ActiveArea.cpp
@@ -8,9 +8,16 @@
 using namespace osp;
 using namespace Urho3D;
 
-ActiveArea::ActiveArea(Context* context) : LogicComponent(context)
+
+ActiveArea::ActiveArea(Context* context) : Satellite(context)
+{
+
+}
+
+ActiveArea::ActiveArea(Context* context, Scene* scn) : ActiveArea(context)
 {
     m_name = "Untitled ActiveArea";
+    m_activeNode = scn;
 
     // load everything within a kilometer
     m_loadRadius = 1000 * 1024;
@@ -21,16 +28,11 @@ ActiveArea::~ActiveArea()
     //Satellite::~Satellite();
 }
 
-void ActiveArea::RegisterObject(Context* context)
-{
-    context->RegisterFactory<ActiveArea>("ActiveArea");
-}
 
-void ActiveArea::FixedUpdate(float timeStep)
+void ActiveArea::update(float timeStep)
 {
-    m_activeNode = node_;
     
-    PhysicsWorld* pw = node_->GetScene()->GetComponent<PhysicsWorld>();
+    PhysicsWorld* pw = m_activeNode->GetComponent<PhysicsWorld>();
 
     LongVector3 focusPosLong;
 
@@ -132,7 +134,7 @@ void ActiveArea::FixedUpdate(float timeStep)
         //URHO3D_LOGINFOF("move!");
         // Translate all nodes to move the camera back to the center
         PODVector<Node*> rigidBodies;
-        node_->GetChildren(rigidBodies);
+        m_activeNode->GetChildren(rigidBodies);
         for (Node* rbNode : rigidBodies)
         {
             rbNode->Translate(offsetDist, TS_WORLD);
@@ -204,7 +206,7 @@ void ActiveArea::distance_check_then_load(Satellite* sat,
         URHO3D_LOGINFOF("ActiveArea Loading: %s", sat->get_name().CString());
 
         Vector3 floatPos(Vector3(relative.x_, relative.y_, relative.z_)
-                       / m_unitsPerMeter);
+                       / (1 << m_precision));
         sat->load(this, floatPos);
     }
 }

src/ActiveArea.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <Urho3D/Scene/LogicComponent.h>
+#include <Urho3D/Scene/Scene.h>
 
 #include "Satellite.h"
 
@@ -10,22 +10,24 @@ namespace osp {
 
 class ActiveArea;
 
+
 /**
  * Turns an ordinary Urho3D scene into a window to the larger OSP universe
  * Loads and unloads Satellites, and handles floating origin
  */
-class ActiveArea : public LogicComponent, public Satellite
+class ActiveArea : public Satellite
 {
-    URHO3D_OBJECT(ActiveArea, LogicComponent)
+
+
+    URHO3D_OBJECT(ActiveArea, Satellite)
 
 public:
 
     ActiveArea(Context* context);
+    ActiveArea(Context* context, Scene* scene);
     ~ActiveArea();
 
-    virtual void FixedUpdate(float timeStep) override;
-
-    static void RegisterObject(Context* context);
+    void update(float timeStep);
 
     //void relocate(AstronomicalBody* body, const LongVector3& localBodyPos);
 

src/AstronomicalBody.cpp
@@ -7,7 +7,7 @@
 
 using namespace osp;
 
-AstronomicalBody::AstronomicalBody()
+AstronomicalBody::AstronomicalBody(Context* context) : Satellite(context)
 {
     m_loadRadius = 5000 * 1024;
     m_radius = 4000.0f;
@@ -21,19 +21,22 @@ AstronomicalBody::~AstronomicalBody()
 
 Node* AstronomicalBody::load(ActiveArea* area, const Vector3& pos)
 {
-    Node* scene = area->GetNode();
+    Satellite::load(area, pos);
+
+    Node* scene = area->get_active_node();
 
     m_activeNode = scene->CreateChild(m_name);
     PlanetTerrain* terrain = m_activeNode->CreateComponent<PlanetTerrain>();
     terrain->initialize(this);
 
     LongVector3 relativePos = Satellite::calculate_relative_position(
-                                        area, this, m_unitsPerMeter);
+                                        area, this, m_precision);
+
 
     Vector3 floatPos;
-    floatPos.x_ = float(relativePos.x_) / m_unitsPerMeter;
-    floatPos.y_ = float(relativePos.y_) / m_unitsPerMeter;
-    floatPos.z_ = float(relativePos.z_) / m_unitsPerMeter;
+    floatPos.x_ = float(relativePos.x_) / (1 << m_precision);
+    floatPos.y_ = float(relativePos.y_) / (1 << m_precision);
+    floatPos.z_ = float(relativePos.z_) / (1 << m_precision);
 
     URHO3D_LOGINFOF("Center at: %f %f %f", floatPos.x_,
                     floatPos.y_, floatPos.z_);

src/AstronomicalBody.h
@@ -12,9 +12,11 @@ namespace osp {
 class AstronomicalBody : public Satellite
 {
 
+    URHO3D_OBJECT(AstronomicalBody, Satellite)
+
 public:
 
-    AstronomicalBody();
+    AstronomicalBody(Context* context);
     ~AstronomicalBody() override;
 
     float get_radius()

src/NodeSat.cpp
@@ -3,7 +3,7 @@
 
 using namespace osp;
 
-NodeSat::NodeSat()
+NodeSat::NodeSat(Context* context) : Satellite(context)
 {
     m_name = "Probably a rocket";
 }
@@ -15,13 +15,15 @@ NodeSat::~NodeSat()
 
 Node* NodeSat::load(ActiveArea* area, const Vector3& pos)
 {
+    Satellite::load(area, pos);
+
     if (m_node.Null())
     {
         return nullptr;
     }
 
     // Move the node into the ActiveArea scene
-    m_node->SetParent(area->GetNode());
+    m_node->SetParent(area->get_active_node());
 
     m_activeNode = m_node;
 

src/NodeSat.h
@@ -10,9 +10,12 @@ namespace osp {
  */
 class NodeSat : public Satellite
 {
+
+    URHO3D_OBJECT(NodeSat, Satellite)
+
 public:
 
-    NodeSat();
+    NodeSat(Context* context);
     ~NodeSat() override;
 
     Node* get_node()

src/OspUniverse.cpp
@@ -134,33 +134,34 @@ void OspUniverse::debug_function(const StringHash which)
         // astroid-sized ridiculously spherical 4km radius bumpy ball
 
         // Set the root of the universe to a default astronomical body
-        m_bigUniverse = new AstronomicalBody();
+        m_bigUniverse = new AstronomicalBody(context_);
 
         // Add some more AstronomicalBody
 
-        AstronomicalBody* moonA = new AstronomicalBody();
+        AstronomicalBody* moonA = new AstronomicalBody(context_);
         moonA->set_position(LongVector3(1024 * 16000, 0, 0));
         m_bigUniverse->add_child(moonA);
 
-        AstronomicalBody* moonB = new AstronomicalBody();
+        AstronomicalBody* moonB = new AstronomicalBody(context_);
         moonB->set_position(LongVector3(-1024 * 16000, 0, 0));
         m_bigUniverse->add_child(moonB);
         
-        AstronomicalBody* moonBA = new AstronomicalBody();
+        AstronomicalBody* moonBA = new AstronomicalBody(context_);
         moonBA->set_position(LongVector3(0, 0, 1024 * 16000));
         moonB->add_child(moonBA);
 
-        AstronomicalBody* moonBAA = new AstronomicalBody();
+        AstronomicalBody* moonBAA = new AstronomicalBody(context_);
         moonBAA->set_position(LongVector3(0, 0, 1024 * 16000));
         moonBA->add_child(moonBAA);
 
         // Creata a NodeSat to represent the player's craft
-        NodeSat* subjectSat = new NodeSat();
+        NodeSat* subjectSat = new NodeSat(context_);
         subjectSat->set_node(scene->GetChild("Subject"));
         m_bigUniverse->add_child(subjectSat);
 
         // Link the Urho scene to the universe just created
-        ActiveArea* area = scene->CreateComponent<ActiveArea>();
+        ActiveArea* area = new ActiveArea(context_, scene);
+        m_activeAreas.Push(WeakPtr<ActiveArea>(area));
         m_bigUniverse->add_child(area);
 
         // Make the ActiveArea follow the player's craft arouynd

src/OspUniverse.h
@@ -30,7 +30,7 @@ class OspUniverse : public Object
     SharedPtr<Satellite> m_bigUniverse;
 
     // List of ActiveAreas
-    Vector<SharedPtr<Scene>> m_activeScenes;
+    Vector<WeakPtr<ActiveArea>> m_activeAreas;
 
     //HashMap<StringHash, SharedPtr<ObjectFactory> > m_machines;
 

src/Satellite.cpp
@@ -1,8 +1,14 @@
 #include <Urho3D/IO/Log.h>
 
+#include "ActiveArea.h"
 #include "Satellite.h"
 
 using namespace osp;
+
+Satellite::Satellite(Context* context) : Object(context)
+{
+
+}
 
 Satellite::~Satellite()
 {
@@ -24,7 +30,7 @@ void Satellite::add_child(Satellite *newChild)
 
 LongVector3 Satellite::calculate_relative_position(const Satellite* from,
                                                 const Satellite* to,
-                                                unsigned unitsPerMeter)
+                                                int precision)
 {
     // Distance to self will never be greater than or less than zero.
     if (from == to)
@@ -65,4 +71,43 @@ LongVector3 Satellite::calculate_relative_position(const Satellite* from,
     }
     
     return posB - posA;
+}
+
+
+LongVector3 Satellite::calculate_position()
+{
+    // If loaded
+    if (is_loaded())
+    {
+        // Set position to the current ActiveArea's position
+        //m_position = m_activeArea->get_position();
+        // Add the float position
+        //m_position += m_activeArea->m_unit
+
+        // TODO: account for m_precision
+
+        // Note: ActiveArea's center is its Scene origin
+
+        // Position relative to ActiveArea in meters
+        Vector3 floatPos = m_activeNode->GetPosition();
+
+        // How much the active area moved while this satellite was loaded
+        LongVector3 activeAreaDisplacement = m_activeArea->get_position()
+                - m_activeAreaLoadedPos;
+
+
+
+    }
+    else
+    {
+
+    }
+}
+
+Node* Satellite::load(ActiveArea *area, const Vector3 &pos)
+{
+    m_activeAreaLoadedPos = area->get_position();
+    m_activeArea = area;
+
+    m_positionLoaded = m_position;
 }

src/Satellite.h
@@ -16,10 +16,14 @@ class ActiveArea;
 /**
  * Base class for any physical object in the large universe
  */
-class Satellite : public RefCounted
+class Satellite : public Object
 {
 
+    URHO3D_OBJECT(Satellite, Object)
+
 public:
+
+    Satellite(Context* context);
 
     virtual ~Satellite();
 
@@ -28,12 +32,12 @@ public:
      * located anywhere
      * @param from [in] Reference frame
      * @param to [in] Satellite to calculate position for
-     * @param unitsPerMeter [in] Desired precision of return value
+     * @param precision [in] Desired precision of return value
      * @return Position of 'to' relative to 'from'
      */
     static LongVector3 calculate_relative_position(const Satellite* from,
                                                    const Satellite* to,
-                                                       unsigned unitsPerMeter);
+                                                   int precision);
     
     /**
      * Add a child Satellite
@@ -42,6 +46,11 @@ public:
     void add_child(Satellite* newChild);
 
     /**
+     * Returns current position. This only returns the previously stored value
+     * for position, but does not calculate a new one. Consider calling
+     * calculate_position() instead, to account for movement due to
+     * Trajectory calculations or movement of the active node if loaded.
+     *
      * @return Position relative to parent
      */
     LongVector3 get_position() const
@@ -50,14 +59,13 @@ public:
     }
 
     /**
-     * Set position relative to parent
+     * Set position relative to parent. This function is probably dangerous
      * @param pos [in] Desired value
      */
     inline void set_position(const LongVector3& pos)
     {
         m_position = pos;
     }
-
 
     inline uint64_t get_load_radius() const
     {
@@ -98,13 +106,30 @@ public:
     }
 
     /**
+     * Shorthand for checking if m_activeNode is null
+     * @return true if loaded, false if not loaded
+     */
+    inline bool is_loaded() const
+    {
+        return m_activeNode.Null();
+    }
+
+    /**
+     * Calculates a new position for this Satellite. Calls the Trajectory for a
+     * new position, or uses the position of the active node when loaded.
+     *
+     * @return Position relative to parent, same as get_position
+     */
+    virtual LongVector3 calculate_position();
+
+    /**
      * Try loading the satellite into the scene
      * Usually called when entering an ActiveArea (player gets close)
      * @param area [in] ActiveArea to load into
      * @param pos [in] Desired position in meters to load into.
      * @return Pointer to Active Node, null if load failed
      */
-    virtual Node* load(ActiveArea* area, const Vector3& pos) = 0;
+    virtual Node* load(ActiveArea* area, const Vector3& pos);
 
     /**
      * Similar to load, called when an object is visible from an ActiveArea,
@@ -153,19 +178,40 @@ protected:
 
     // Associated node when loaded by an ActiveArea
     // Can only be loaded into one ActiveArea at a time
+    // Null if not loaded
     WeakPtr<Node> m_activeNode;
+
+    // ActiveArea loaded into
+    WeakPtr<ActiveArea> m_activeArea;
+
+    // Position of the ActiveArea above the moment its loaded. Used to
+    // calculate m_position quickly
+    LongVector3 m_activeAreaLoadedPos;
+
+    // Capture of m_position taken on load
+    LongVector3 m_positionLoaded;
 
     // Preview nodes beloning to many different ActiveAreas
     // Multiple ActiveAreas can see the same Satallite from a distance
     //Vector< WeakPtr<Node> > m_previews;
 
-    // Scale for how many LongVector3 units is equal to a meter. This applies
-    // to this Satellite's children. If this was 1000, then 1 = 1mm
-    // smaller number means less precision, but larger
-    unsigned m_unitsPerMeter = 1024;
+    // Not implemented yet, but it's here
+    // Scale for how many LongVector3 units is equal to a meter. This only
+    // applies to this Satellite's children.
+    //
+    // m_precision bit shifts m_position, multiplying it by powers of two
+    //
+    // precision = 0 ->   1  units = 1 meter
+    // precision = 1 ->   2  units = 1 meter
+    // precision = 2 ->   4  units = 1 meter
+    // ...
+    // precision = 10 ->  1024 units = 1 meter (The Default)
+    //
+    // This should be able to go negative for very large sizes
+    int m_precision = 10;
 
     // orbital stuff goes here. something like:
-    // OrbitType m_orbitType
+    // Trajectory m_trajectory;
 
 };
 

src/main.cpp
@@ -48,7 +48,6 @@
 #include <Urho3D/AngelScript/APITemplates.h>
 #include <Urho3D/ThirdParty/AngelScript/angelscript.h>
 
-#include "ActiveArea.h"
 #include "config.h"
 #include "GLTFFile.h"
 #include "Machine.h"
@@ -77,7 +76,7 @@ public:
     OSPApplication(Context * context) : Application(context), m_framecount(0),
                                         m_time(0)
     {
-        ActiveArea::RegisterObject(context);
+        //ActiveArea::RegisterObject(context);
         //AstronomicalBody::RegisterObject(context);
         //Entity::RegisterObject(context);
         GLTFFile::RegisterObject(context);